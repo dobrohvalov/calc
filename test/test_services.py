82188d7092776802acea442f81d0d9c85fedbb84
fovenelian <info@f-lab.ru>
2022-02-18 17:24:37 +0300

    Corrected the ridmi file

--- test/test_services.py
+++ test/test_services.py
@@ -2,6 +2,7 @@ import pytest
 
 from app.services import calculate
 
+
 def test_calculate():
     response = calculate("[+](7.322)[(*)(12.32233)]0")
     assert response == {
@@ -10,6 +11,7 @@ def test_calculate():
     "status": "OK"
     }
 
+
 def test_calculate_by_zero():
     response = calculate("[+](7.322)[(*)(0)]2")
     assert response == {
@@ -17,6 +19,7 @@ def test_calculate_by_zero():
     "response": 0.0,
     "status": "OK"
     }
+
 
 def test_calculate_by_text():
     response = calculate("[+](+7.322)[(*jj)(-0)]2")
